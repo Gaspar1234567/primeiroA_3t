0225dc5294dd5810f76e14b4ee7a86ed620d27c8
Gaspar1234567 <110241742+Gaspar1234567@users.noreply.github.com>
2022-10-07 11:07:50 -0300

    AAAAA

--- estudo.js
+++ estudo.js
@@ -1,1 +1,7 @@
 
+function calcula(){
+     let c = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let r = c * (j/100);
+document.white ("Resultado" + r);
+}
