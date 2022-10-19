1ec3f24f7a2103c56a5f3505851479e776798da0
Gaspar1234567 <110241742+Gaspar1234567@users.noreply.github.com>
2022-10-19 08:22:06 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -5,3 +5,30 @@ function calcula(){
     let r = c * (j/100);
 document.white ("Resultado" + r);
 }
+
+document.write("resultado: R$"+)
+}
+
+let operacao = "";
+function operacao(op){
+    operaçao = op;
+}
+function calcule (){
+    let v1 =document.getElementById("v1")
+}
+
+
+
+
+
+
+
+
+function calcule (){
+    let v1 = document.getElementById ("v1").value;
+    let v2 = document. getElementById
+    let r = 0;
+    if (operacao === "+"){
+        r = Number (v1) + Number (v2);
+    }
+    Document.getElementByI("resultado").value = r;
